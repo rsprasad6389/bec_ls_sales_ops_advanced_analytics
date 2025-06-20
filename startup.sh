f6c66d0f18f39cb5b21e53f92e35acb4b771b7b8
rsprasad6389 <rsprasad@beckman.com>
2025-06-20 19:10:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-python -m venv antenv
+# python -m venv antenv
+# source antenv/bin/activate
+# # pip install --upgrade pip
+# # pip install -r requirements.txt
+# python -m streamlit run PSP_GPT_v5.py --server.port=$PORT --server.enableCORS=false
+
+#!/bin/bash
 source antenv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python -m streamlit run PSP_GPT_v5.py --server.port=$PORT --server.enableCORS=false
+exec python -m streamlit run PSP_GPT_v5.py --server.port=$PORT --server.enableCORS=false
+
